cddccd18d3af2320daee5b0d48734d119addd978
Rishi Ghai <rishighai97@gmail.com>
2025-03-05 18:19:47 +0530

    Updated datetime_utils

--- statement-loader/utils/datetime_utils.py
+++ statement-loader/utils/datetime_utils.py
@@ -3,5 +3,6 @@ from datetime import datetime
 def validate_datetime(datetime_string: str, format: str):
     try:
         datetime.strptime(datetime_string, format)
+        return True
     except ValueError:
-        raise ValueError(f"Incorrect data format, should be {format}")
+        return False
