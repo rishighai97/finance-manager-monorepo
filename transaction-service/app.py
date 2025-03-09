7f832857dfc9acbbc840606190370b1c15424b15
Rishi Ghai <rishighai97@gmail.com>
2025-03-09 20:12:28 +0530

    added api exception handler

--- transaction-service/app.py
+++ transaction-service/app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-from controller import statement_upload_controller
 from exception import api_exception_handlers
 
 
@@ -11,7 +10,7 @@ def setup_app():
     global app
     app = Flask(__name__)
     app.register_blueprint(api_exception_handlers.blueprint)
-    app.register_blueprint(statement_upload_controller.blueprint)
+    # app.register_blueprint(statement_upload_controller.blueprint)
 
 def run_app():
     app.run(host=os.getenv("TRANSACTION_SERVICE_SERVER_HOST"), port=os.getenv("TRANSACTION_SERVICE_SERVER_PORT"))
