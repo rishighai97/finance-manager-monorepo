ce22d565e5bce1c814d69ef5b5b5702c64c9625b
Rishi Ghai <rishighai97@gmail.com>
2025-04-02 12:45:01 +0530

    calculated opening anbd closing balance

--- transaction-service/src/main/java/com/finance/manager/transaction_service/service/TransactionServiceImpl.java
+++ transaction-service/src/main/java/com/finance/manager/transaction_service/service/TransactionServiceImpl.java
@@ -16,6 +16,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Service
@@ -27,35 +28,61 @@ public class TransactionServiceImpl implements TransactionService {
     @Override
     public TransactionsDto getUserTransactions(List<Integer> userAccountIds, String startDate, String endDate) {
         List<Transaction> transactionList = transactionDao.fetchAll(userAccountIds, startDate, endDate);
-//        Map<Integer, List<Transaction>> userAccountIdToTransactionListMap = transactionList
-//                .stream()
-//                .collect(Collectors.groupingBy(Transaction::userAccountId));
-        BigDecimal totalDebitAmount = getTotalDebitOrCreditAmount(transactionList, DebitCreditIndicator.DEBIT);
-        BigDecimal totalCreditAmount = getTotalDebitOrCreditAmount(transactionList, DebitCreditIndicator.CREDIT);
         return TransactionsDto
                 .builder()
                 .startDate(startDate)
                 .endDate(endDate)
-                .totalDebit(totalDebitAmount)
-                .totalCredit(totalCreditAmount)
+                .totalDebit(getTotalDebitOrCreditAmount(transactionList, DebitCreditIndicator.DEBIT))
+                .totalCredit(getTotalDebitOrCreditAmount(transactionList, DebitCreditIndicator.CREDIT))
+                .openingBalance(calculateOpeningBalance(transactionList))
+                .closingBalance(calculateClosingBalance(transactionList))
                 .transactions(transactionList)
                 .build();
+    }
+
+    private BigDecimal calculateClosingBalance(List<Transaction> transactionList) {
+        return getEarliestOrLatestTransactionPerUserAccountId(transactionList, false)
+                .map(Transaction::closingBalance)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private BigDecimal calculateOpeningBalance(List<Transaction> transactionList) {
+        return getEarliestOrLatestTransactionPerUserAccountId(transactionList, true)
+                .map(t-> {
+                    BigDecimal closingBalance = t.closingBalance();
+                    BigDecimal debitOrCreditAmount = t.debitOrCreditAmount();
+                    boolean credit = t.isDebitOrCredit().equalsIgnoreCase(DebitCreditIndicator.CREDIT.getName());
+                    return credit ? closingBalance.subtract(debitOrCreditAmount) : closingBalance.add(debitOrCreditAmount);
+                })
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+
+    }
+
+    private Stream<Transaction> getEarliestOrLatestTransactionPerUserAccountId(List<Transaction> transactionList, boolean earliest) {
+        return transactionList
+                .stream()
+                .collect(Collectors.groupingBy(Transaction::userAccountId))
+                .values()
+                .stream()
+                .map(transactions -> getTransactionOnMinOrMaxDate(transactions, earliest));
     }
 
     private BigDecimal getTotalDebitOrCreditAmount(List<Transaction> transactionList, DebitCreditIndicator debitOrCreditIndicator) {
         return transactionList
                 .stream()
-                .map(t->Objects.nonNull(t) && t.isDebitOrCredit().equalsIgnoreCase(debitOrCreditIndicator.getName()) ? t.debitOrCreditAmount() : BigDecimal.ZERO)
+                .filter(t->Objects.nonNull(t) && Objects.nonNull(t.isDebitOrCredit()) && t.isDebitOrCredit().strip().equalsIgnoreCase(debitOrCreditIndicator.getName()))
+                .map(Transaction::debitOrCreditAmount)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
-//    private BigDecimal getTransactionOnMinOrMaxDate(List<Transaction> transactions, boolean minDate) {
-//        transactions
-//                .stream()
-//                .sorted(Comparator.comparing(this::getDateStringAndConvertToLocalDate, minDate ? Comparator.reverseOrder(): Comparator.naturalOrder()))
-//                .findFirst()
-//                .orElseThrow(()-> throw new RuntimeException("Unable to get transaction at min / max date"));
-//    }
+    private Transaction getTransactionOnMinOrMaxDate(List<Transaction> transactions, boolean earliest) {
+        return transactions
+                .stream()
+                .sorted(Comparator.comparing(this::getDateStringAndConvertToLocalDate, earliest ? Comparator.naturalOrder() : Comparator.reverseOrder()))
+                .findFirst()
+                .orElseThrow(()-> new RuntimeException("Unable to get transaction at min / max date"));
+    }
 
     private LocalDate getDateStringAndConvertToLocalDate(Transaction t1) {
         return LocalDate.parse(t1.date(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
