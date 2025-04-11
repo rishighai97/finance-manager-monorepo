b41c179fc191b1b5133750437892b8767dbb5008
Rishi Ghai <rishighai97@gmail.com>
2025-04-11 11:52:58 +0530

    updated sql

--- statement-loader/resources/sql/9. transaction_user_category.sql
+++ statement-loader/resources/sql/9. transaction_user_category.sql
@@ -4,7 +4,7 @@ CREATE SEQUENCE transaction_user_category_sequence START 1;
 CREATE TABLE transaction_user_category  (
   id int PRIMARY KEY DEFAULT nextval('transaction_user_category_sequence'),
   transaction_id text references transaction,
-  user_category_id serial references user_detail,
+  user_category_id serial references user_category,
   unique(transaction_id, user_category_id)
 );
 
