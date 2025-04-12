b66c43b9d919593b97a7fd6835965b4c90138a74
Rishi Ghai <rishighai97@gmail.com>
2025-04-12 18:24:19 +0530

    claude scripts for ubuntu local machine

--- scripts/local_startup/ubuntu_rishi/claude-patch.sh
+++
@@ -1,3 +0,0 @@
-cd /home/rishi/Desktop/finance-manager-application/.venv/bin/
-source activate
-python /home/rishi/Desktop/finance-manager-application/scripts/claude/claude_patch.py ui
---
+++ scripts/local_startup/ubuntu_rishi/claude/claude-get-context.sh
@@ -0,0 +1,3 @@
+cd /home/rishi/Desktop/finance-manager-application/.venv/bin/
+source activate
+python /home/rishi/Desktop/finance-manager-application/scripts/local_startup/ubuntu_rishi/claude/claude_get_context.py
