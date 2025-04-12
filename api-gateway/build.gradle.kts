b30071560f1cb33c848d723170c69cc7b5f40e93
rishighai97@gmail.com <rishighai97@gmail.com>
2025-04-12 19:11:56 +0530

    disabling gateway

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -28,8 +28,8 @@ repositories {
 extra["springCloudVersion"] = "2025.0.0-M3"
 
 dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+//	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
