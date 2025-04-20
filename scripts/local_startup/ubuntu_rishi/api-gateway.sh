0fb0cb8c4c157576226bc7f4f811b5cf36f594d3
rishighai97@gmail.com <rishighai97@gmail.com>
2025-04-20 18:45:14 +0530

    updated script

--- scripts/local_startup/ubuntu_rishi/api-gateway.sh
+++ scripts/local_startup/ubuntu_rishi/api-gateway.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/home/rishi/.jdks/azul-23.0.2
+export JAVA_HOME=/home/rishi/.jdks/openjdk-21.0.1
 export PATH=$JAVA_HOME/bin:$PATH
 cd /home/rishi/Desktop/finance-manager-application/api-gateway
 ./gradlew bootrun --args='--spring.profiles.active=local'
