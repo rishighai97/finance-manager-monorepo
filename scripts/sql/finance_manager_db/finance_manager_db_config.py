9958b104ec3b0978bcdd636cd9a84c12c92fca5f
rishighai97@gmail.com <rishighai97@gmail.com>
2025-04-21 07:45:00 +0530

    increasing transaction title size

--- scripts/sql/finance_manager_db/finance_manager_db_config.py
+++ scripts/sql/finance_manager_db/finance_manager_db_config.py
@@ -155,7 +155,7 @@ finance_manager_db_config = [
                 id text PRIMARY KEY NOT NULL UNIQUE, 
                 date date not null, 
                 user_account_id serial not null references user_account, 
-                title varchar(100) not null, amount numeric not null, 
+                title varchar(500) not null, amount numeric not null, 
                 debit_credit_indicator varchar(2) not null, 
                 closing_balance numeric, 
                 category_id varchar(200), 
