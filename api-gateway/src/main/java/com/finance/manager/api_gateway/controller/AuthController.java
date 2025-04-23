0cac76fa01aeee957a4dbefd08a2c62b1e4e2bae
rishighai97@gmail.com <rishighai97@gmail.com>
2025-04-23 16:31:06 +0530

    added loggers

--- api-gateway/src/main/java/com/finance/manager/api_gateway/controller/AuthController.java
+++ api-gateway/src/main/java/com/finance/manager/api_gateway/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.finance.manager.api_gateway.dto.UserSignupRequest;
 import com.finance.manager.api_gateway.dto.UserDetails;
 import com.finance.manager.api_gateway.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import org.springframework.web.client.HttpClientErrorException;
 @RequestMapping("/auth")
 @CrossOrigin
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
 
     private final UserService userService;
@@ -27,7 +29,9 @@ public class AuthController {
     @PostMapping("/signup")
     public ResponseEntity<UserDetails> signup(@RequestBody UserSignupRequest request) {
         try {
+            log.info("Received signup request for user - {}", request.getUsername());
             UserDetails userDetails = userService.createUser(request);
+            log.info("Signup successful for user - {}", request.getUsername());
             return ResponseEntity.ok(userDetails);
         } catch (HttpClientErrorException e) {
             return ResponseEntity.status(HttpStatusCode.valueOf(e.getStatusCode().value())).build();
@@ -37,7 +41,9 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody UserLoginRequest request) {
         try {
+            log.info("Received login request for user - {}", request.getUsername());
             AuthResponse response = userService.authenticateUser(request);
+            log.info("Signup successful for user - {}", request.getUsername());
             return ResponseEntity.ok(response);
         } catch (HttpClientErrorException e) {
             return ResponseEntity.status(HttpStatusCode.valueOf(e.getStatusCode().value())).build();
